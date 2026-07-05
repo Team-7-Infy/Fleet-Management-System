a7abbf21e5cf2f9d6f2f5c03562f898a5cf08297
Veer <veerk.dev@gmail.com>
2026-07-05 15:37:57 +0530

    Change misc cost color from gray to purple in expenditure pie chart

--- FMS/FMS/Modules/FleetManager/ViewModels/ReportsViewModel.swift
+++ FMS/FMS/Modules/FleetManager/ViewModels/ReportsViewModel.swift
@@ -204,7 +204,7 @@ final class ReportsViewModel: ObservableObject {
             ExpenditureSlice(label: "Labour", amount: maintenanceLabourTotal, color: FleetPalette.accent),
             ExpenditureSlice(label: "Parts", amount: maintenancePartsTotal, color: FleetPalette.warning),
             ExpenditureSlice(label: "Fuel", amount: filteredTripFuelTotal, color: FleetPalette.success),
-            ExpenditureSlice(label: "Misc", amount: filteredTripMiscTotal, color: .gray)
+            ExpenditureSlice(label: "Misc", amount: filteredTripMiscTotal, color: .purple)
         ]
     }
 
